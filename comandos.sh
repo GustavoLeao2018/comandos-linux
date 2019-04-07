3b5f221c694ba5d9d0ece04f299e93e922d32622
GustavoLeao2018 <gustavo.leao.nogueira.2017@gmail.com>
2019-04-06 23:14:35 -0300

    comandos

---
+++ comandos.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cvt 1366 768 60
+xrandr --newmode "1368x768_60.00"   85.25  1368 1440 1576 1784  768 771 781 798 -hsync +vsync
+xrandr --addmode VGA1 1368x768_60.00
+xrandr --output VGA1 --mode 1368x768_60.00
